b18f5d70e783d5be0bf32d6ab2b59d5b941d781a
Elyrene <3222956277@qq.com>
2026-09-19 16:04:49 +0800

    🎉 init: 图书馆项目后端初始化

---
+++ Serveice/src/main/java/com/Library/serveice/ServeiceApplication.java
@@ -0,0 +1,13 @@
+package com.Library.serveice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeiceApplication.class, args);
+    }
+
+}
