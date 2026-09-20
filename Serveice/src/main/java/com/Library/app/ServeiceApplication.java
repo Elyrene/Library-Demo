1be76f49a6cb5f3a7c291162afd645cc5efd64fe
Elyrene <3222956277@qq.com>
2026-09-20 18:01:09 +0800

    🔧 build: 添加 Mybatis 依赖，构建后端框架

--- Serveice/src/main/java/com/Library/app/ServeiceApplication.java
+++ Serveice/src/main/java/com/Library/app/ServeiceApplication.java
@@ -1,9 +1,11 @@
-package com.Library.serveice;
+package com.Library.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.Library")
+@MapperScan("com.Library.mapper")
 public class ServeiceApplication {
 
     public static void main(String[] args) {
--- Serveice/src/test/java/com/Library/app/ServeiceApplicationTests.java
+++ Serveice/src/test/java/com/Library/app/ServeiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Library.serveice;
+package com.Library.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
