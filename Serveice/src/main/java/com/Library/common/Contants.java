d37e333901651b94b7640927bfdd1564c0832196
Elyrene <3222956277@qq.com>
2026-09-23 22:54:23 +0800

    ✨ feat: 添加 Session 常量

---
+++ Serveice/src/main/java/com/Library/common/Contants.java
@@ -0,0 +1,13 @@
+package com.Library.common;
+
+import lombok.Data;
+
+@Data
+public final class Contants {
+
+    private Contants(){}
+
+    public static  final String SESSION_USER = "user";
+    public static  final String SESSION_TYPE = "type";
+
+}
